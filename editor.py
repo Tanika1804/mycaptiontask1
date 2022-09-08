25dbc873a1a8d5a1fc6d03e12e6f23c660adc8bb
Tanika1804 <113040763+Tanika1804@users.noreply.github.com>
2022-09-08 19:09:55 +0530

    Add files via upload

---
+++ editor.py
@@ -0,0 +1,4 @@
+import math as M  
+Radius = float (input ("Input the radius of the circle: "))  
+area_of_the_circle = M.pi* Radius * Radius  
+print (" The area of the circle with the radius 1.1 is: ", area_of_the_circle)  
